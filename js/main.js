4a1831df4be89a17bdb020403872456aa8d1ccd7
D4rkm4n <59933790+D4rkm4n215@users.noreply.github.com>
2025-06-02 23:17:05 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -22,7 +22,7 @@ async function login() {
 
     try {
         const encodedToken = encodeURIComponent(token);
-        const res = await fetch("http://130.61.19.194:8080/api/register?token=" + encodedToken, {
+        const res = await fetch("https://130.61.19.194:8080/api/register?token=" + encodedToken, {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ username, password })
@@ -50,7 +50,7 @@ async function signup() {
 
     try {
         const encodedToken = encodeURIComponent(token);
-        const res = await fetch("http://130.61.19.194:8080/api/register?token=" + encodedToken, {
+        const res = await fetch("https://130.61.19.194:8080/api/register?token=" + encodedToken, {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ username, password })
